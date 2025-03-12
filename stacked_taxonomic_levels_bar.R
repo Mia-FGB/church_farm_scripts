52fa748a179209c65b455697aa64cd62a4507853
Mia-FGB <92913584+Mia-FGB@users.noreply.github.com>
2025-03-12 12:06:51 +0000

    Created a new script to look at the CIMMYT disease data

--- stacked_taxonomic_levels_bar.R
+++ stacked_taxonomic_levels_bar.R
@@ -59,7 +59,7 @@ p <- ggplot(
   scale_x_date(breaks = break.vec, date_labels = "%d-%b")
 
 # Save as SVG
-ggsave(filename = "Regular_Collections_2023/graphs/stacked_phylum_graph.svg", plot = p, width = 8, height = 6)
+#ggsave(filename = "Regular_Collections_2023/graphs/stacked_phylum_graph.svg", plot = p, width = 8, height = 6)
 
 #------------------
 
@@ -126,15 +126,26 @@ path <- ggplot(
 
 
 # Including Unclassified -------------------------------------------------
+#Phylum level first ----
 
 #Create filtered datasets with the same columns so I can join them altogether for plotting
-#nOte if I change the filtering I need to remake the phylum_data and classified_reads datasets 
-
+#note if I change the filtering I need to remake the phylum_data and classified_reads datasets 
 #Normal phylum data, filtered to be rel_abundance > 0.1
 phylum_data_uncl <- data %>%
   filter(NCBI.Rank == "phylum") %>% #2301 entries, many with a 0 read count
   filter(rel_abundance > 0.01) %>%  #reduces entries can change the stringency
   select(Sample, Date.collected, read.count, NumContigs, phylum)
+
+# Group and aggregate the candidatus data which has two species classified as phylum?
+phylum_data_uncl <- phylum_data_uncl %>%
+  # Modify 'phylum' to group the selected rows into "Candidatus"
+  mutate(phylum = ifelse(phylum %in% c("Candidatus Binatota", "Candidatus Krumholzibacteriota"), 
+                         "Candidatus", phylum)) %>%
+  # Group by Sample
+  group_by(Sample, phylum, Date.collected, NumContigs) %>%
+  # Sum read counts within each group
+  summarise(read.count = sum(read.count), .groups = "drop")  # Keep other columns as needed
+
 
 # Ensure all phyla are present in each sample, filling missing values with 0
 # This is how I am creating the averages whilst only plotting those with rel_abundance >0.01
@@ -152,14 +163,14 @@ classified_data <- data %>%
   filter(NCBI.Rank == "phylum", rel_abundance <= 0.01) %>%  # Entries that did not pass the filter
   group_by(Sample, Date.collected, NumContigs) %>%  # Group by sample
   summarise(read.count = sum(read.count)) %>%  # Sum the reads not passing the filter
-  mutate(phylum = "classified") 
+  mutate(phylum = "Classified") 
 
 # unclassified reads - calculated by deducting the total of the phylum level classified reads from NumContigs
 unclassified_data <- data %>% 
   group_by(Sample, Date.collected, NumContigs) %>% 
   filter(NCBI.Rank == "phylum") %>% 
   summarise(total_class_reads = sum(read.count)) %>% #sum up the total phylum level classified reads
-  mutate(read.count = NumContigs - total_class_reads, phylum = "unclassified") %>% 
+  mutate(read.count = NumContigs - total_class_reads, phylum = "Unclassified") %>% 
   select(Sample, Date.collected, read.count, NumContigs, phylum)
 
 # concatenate the datasets together
@@ -178,12 +189,29 @@ full_data_avg <-  full_data %>%
 
 full_data_avg$Date.collected = as.Date(full_data_avg$Date.collected)
 
-#Plotting phylum level data with unclassifieds ---
+# Set custom order for phylum factor
+full_data_avg <- full_data_avg %>%
+  mutate(phylum = factor(phylum, 
+                         levels = c(sort(setdiff(unique(phylum), c("Classified", "Unclassified"))), 
+                                    "Classified", "Unclassified")))
+#To get the colour palette --
 
-ggplot(
+# Identify unique phyla and exclude "classified" and "unclassified"
+phyla <- sort(setdiff(unique(full_data_avg$phylum), c("Classified", "Unclassified")))
+# Define color palette for the remaining phyla
+n_phyla <- length(phyla)
+main_palette <- colorRampPalette(brewer.pal(12, "Set3"))(n_phyla)
+# Combine main palette with black and grey for "classified" and "unclassified"
+custom_colors <- c(setNames(main_palette, phyla), 
+                   "Classified" = "#D3D3D3", 
+                   "Unclassified" = "#4F4F4F")
+
+#Plotting phylum level data with classified & unclassified ---
+up <- ggplot(
   full_data_avg, aes(x = Date.collected, y = mean_abundance, fill = phylum)) + 
   geom_bar(stat = "identity", color = "black", size = 0.3,) + #Using position = fill makes the bar fill the axis
-  ylab("Relative Abundance (> 0.15%)") +
+  scale_fill_manual(values = custom_colors) + 
+  ylab("Relative Abundance (> 0.01%)") +
   xlab("Date Collected") +
   ggtitle("Phylum Composition over time") +
   scale_y_continuous(expand = c(0, 0)) +
@@ -196,7 +224,70 @@ ggplot(
     axis.line = element_line(size = 0.7)) + #thicker axis
   scale_x_date(breaks = break.vec, date_labels = "%d-%b")
 
-#Still to change:
-# - Nicer colours and phylum order
-# - Try plotting monthly and weekly data seperately 
-# - Maybe plot the monthly data with a representative from the weekly samples
+#ggsave(filename = "Regular_Collections_2023/graphs/stacked_phylum_graph_unclassified.svg", plot = up, width = 15, height = 6)
+
+#Monthly phylum plot ---------
+#also selecting samples from the weekly data that are closest to a month on
+#will all have to change once I get the actual dates
+month_dates <- as.Date(
+  c("2022-10-03", "2022-11-14", "2022-12-14", "	2023-01-14",
+    "2023-02-14", "2023-03-14", "2023-04-14", "2023-05-25",
+    "2023-06-14", "2023-07-12", "2023-08-17"))
+
+# Filter to include only rows with dates in `month_dates`
+month_data <- full_data_avg %>%
+  filter(Date.collected %in% month_dates)  %>%
+  mutate(Date.collected = factor(Date.collected, levels = month_dates))
+
+#Plot
+month <- ggplot(
+  month_data, aes(x = Date.collected, y = mean_abundance, fill = phylum)) + 
+  geom_bar(stat = "identity", color = "black", size = 0.3,) + #Using position = fill makes the bar fill the axis
+  scale_fill_manual(values = custom_colors) + 
+  ylab("Relative Abundance (> 0.01%)") +
+  xlab("Date Collected") +
+  ggtitle("Monthly Phylum Composition") +
+  scale_y_continuous(expand = c(0, 0)) +
+  theme_bw() + 
+  theme(
+    panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+    panel.border = element_blank(), 
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=0.5),
+    axis.title.x = element_blank(), #remove x-axis title
+    axis.line = element_line(size = 0.7)) #thicker axis
+
+#ggsave(filename = "Regular_Collections_2023/graphs/month_stacked_phylum_graph_unclassified.svg", plot = month, width = 8, height = 6)
+
+#Weekly Phylum-------
+week_dates <- as.Date(
+  c("2023-05-25", "2023-06-02", "2023-06-06", "2023-06-14",
+    "2023-06-21","2023-06-30", "2023-07-07", "2023-07-12",
+    "2023-07-20", "2023-07-27", "2023-08-03", "2023-08-09",
+    "2023-08-17"))
+
+# Filter to include only rows with dates in `month_dates`
+week_data <- full_data_avg %>%
+  filter(Date.collected %in% week_dates)  %>%
+  mutate(Date.collected = factor(Date.collected, levels = week_dates))
+
+
+#Plot
+week <- ggplot(
+  week_data, aes(x = Date.collected, y = mean_abundance, fill = phylum)) + 
+  geom_bar(stat = "identity", color = "black", size = 0.3,) + #Using position = fill makes the bar fill the axis
+  scale_fill_manual(values = custom_colors) + 
+  ylab("Relative Abundance (> 0.01%)") +
+  xlab("Date Collected") +
+  ggtitle("Weekly Phylum Composition") +
+  scale_y_continuous(expand = c(0, 0)) +
+  theme_bw() + 
+  theme(
+    panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+    panel.border = element_blank(), 
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=0.5),
+    axis.title.x = element_blank(), #remove x-axis title
+    axis.line = element_line(size = 0.7)) #thicker axis
+
+#ggsave(
+#  filename = "Regular_Collections_2023/graphs/week_stacked_phylum_graph_unclassified.svg",
+#       plot = week, width = 8, height = 6)
