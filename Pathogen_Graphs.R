d5ef5a91d5f67fcd7a03b09d063e67f9fafc169c
Mia-FGB <92913584+Mia-FGB@users.noreply.github.com>
2025-04-01 13:45:26 +0100

    pathogen bar plot with data from multiple years on the same axis

--- Pathogen_Graphs.R
+++ Pathogen_Graphs.R
@@ -57,8 +57,6 @@ pathogen_meta <- pathogen_meta %>%
 
 
 # Plotting --------
-
-
 plot_pathogen_bar <- function(years, name){
   filtered_data <- pathogen_meta %>% filter(Year %in% years, Name == name)
   
@@ -117,3 +115,62 @@ for (name in pathogens) {
   plot_pathogen_bar(years = c(2022, 2023), 
                     name = name)
 }
+
+
+# Multiple years on the same axis --------
+# Is working but in order for the bars to have a nice width, have Sample on x-axis which is a bit messy
+# Tried using the date-formtatitng approach but it looked worse
+
+pathogen_meta$Date_No_Yr <- format(pathogen_meta$DateTime_UTC, "1900-%m-%d")
+# Convert Date_No_Yr to a proper date format (assuming it's in MM-DD format)
+pathogen_meta$Date_No_Yr <- as.Date(pathogen_meta$Date_No_Yr, format = "%Y-%m-%d")
+
+plot_pathogen_bar_yrs <- function(name){
+  filtered_data <- pathogen_meta %>% filter(Name == name)
+  
+  # Compute mean HPM for each Sample_Time
+  summary_data <- filtered_data %>%
+    group_by(Sample, Date_No_Yr, Year) %>%
+    summarise(
+      Avg_HPM = mean(HPM),
+      SD_HPM = sd(HPM),  # Standard deviation
+      SE_HPM = SD_HPM / sqrt(n()),  # Standard error
+      .groups = "drop"
+    ) %>% arrange(Date_No_Yr)
+  
+  # Ensure Sample is uniquely ordered by Sample
+  summary_data$Sample <- factor(summary_data$Sample, levels = unique(summary_data$Sample))
+  
+  pathogen_plot <- ggplot(summary_data, aes(x = Sample, y = Avg_HPM, fill = as.factor(Year))) +
+    geom_bar(stat = "identity", position = "identity", alpha = 0.7) +
+    geom_errorbar(aes(ymin = Avg_HPM - SE_HPM, ymax = Avg_HPM + SE_HPM), color = "black") +
+    theme(
+      panel.grid.major = element_blank(),  # Remove major grid lines
+      panel.grid.minor = element_blank(),  # Remove minor grid lines
+      axis.line = element_line(color = "black", linewidth = 0.8),  # Make axis lines thicker and black
+      panel.border = element_blank()  # Remove the panel border
+    ) +
+    scale_y_continuous(expand = c(0, 0)) +  # Set the y-axis to start at 0
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 0.5)) +
+    labs(title = paste(name),
+         x = "Date Collected",
+         y = "Average HPM",
+         fill = "Year")
+  
+  # filename <- paste0("~/Library/CloudStorage/OneDrive-NorwichBioScienceInstitutes/Air_Samples/Church_farm/Graphs/pathogen_graphs/", 
+  #                   name, "_bar.svg")
+  # ggsave(filename, plot = pathogen_plot, device = "svg", width = 10, height = 6)
+  print(pathogen_plot)
+}
+
+plot_pathogen_bar_yrs("Zymoseptoria")
+
+# Loop through each pathogen name
+for (name in pathogens) {
+  # Call the function with the current month, year, and pathogen name
+  plot_pathogen_bar_yrs(name)
+}
+
+print(pathogens)
+
+
